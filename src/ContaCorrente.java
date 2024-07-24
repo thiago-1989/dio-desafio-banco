402faeeea2748da143eba8e727069626e375ed73
Thiago Oliveira <thiagodevoliveira@hotmail.com>
2024-07-23 23:27:07 -0300

    Subindo o codigo do desafio

---
+++ src/ContaCorrente.java
@@ -0,0 +1,19 @@
+
+public class ContaCorrente extends Conta {
+
+
+    public ContaCorrente(Cliente cliente) {
+        super(cliente);
+    }
+
+    @Override
+    public void extrato() {
+        System.out.println("\n=== Extrato da Conta Corrente ===");
+        super.infoExtrato();
+    }
+
+    public void saldo() {
+        System.out.println("=== Saldo do Conta Corrente ===");
+        super.infoSaldo();
+    }
+}
